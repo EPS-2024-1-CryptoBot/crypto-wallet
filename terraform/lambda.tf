2939f8d841093d1eebd178f18607fe50eb53cc77
Victor Buendia <victorbuendia03@gmail.com>
2024-04-19 17:53:10 -0300

    Add Terraform configuration for AWS Lambda function in lambda.tf

---
+++ terraform/lambda.tf
@@ -0,0 +1,10 @@
+resource "aws_lambda_function" "terraform_lambda_func" {
+    s3_bucket                      = aws_s3_bucket.wallet_api_zip.id
+    s3_key                         = aws_s3_object.file_upload.key
+    function_name                  = "Wallet_API_Backend"
+    role                           = aws_iam_role.wallet_api_role.arn
+    handler                        = "main.handler"
+    runtime                        = "python3.8"
+    depends_on                     = [aws_iam_role_policy_attachment.Wallet_API_Attach]
+    source_code_hash               = base64sha256(aws_s3_object.file_upload.key)
+}
