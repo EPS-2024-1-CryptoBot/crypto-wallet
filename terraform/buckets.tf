c2533eb3ba1e17600fac9abb7645ffe859386878
Victor Buendia <victorbuendia03@gmail.com>
2024-04-19 17:52:49 -0300

    Add Terraform configuration for AWS S3 bucket and object in buckets.tf

---
+++ terraform/buckets.tf
@@ -0,0 +1,10 @@
+resource "aws_s3_bucket" "wallet_api_zip" {
+  bucket = "prod-backend-cryptobotunb-${data.aws_caller_identity.current.account_id}"
+}
+
+resource "aws_s3_object" "file_upload" {
+  bucket      = aws_s3_bucket.wallet_api_zip.id
+  key         = "lambda_zip.zip"
+  source      = "${path.root}/../lambda_function.zip"
+  source_hash = filemd5("${path.root}/../lambda_function.zip")
+}
