a539c8640320269224ffd92d542b8db645f267c8
Victor Buendia <victorbuendia03@gmail.com>
2024-04-19 18:02:39 -0300

    Add new endpoint for home page in main.py

--- wallet/main.py
+++ wallet/main.py
@@ -7,11 +7,18 @@ handler = Mangum(app)
 
 @app.get("/")
 def read_root():
+    """
+    This is the root path of the API
+    """
     return {"WalletAPI": "CryptoBot_UnB_2024.1"}
 
 @app.get("/search")
 def search(query: str):
     return {"Search": "You searched for {query}".format(query=query)}
+
+@app.get("/home")
+def search(query: str):
+    return {"Welcome Home!": "You are always welcome here."}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
