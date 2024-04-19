da4a571c4aa81406d8ec2a8b5153323ac73ebf62
Victor Buendia <victorbuendia03@gmail.com>
2024-04-19 17:52:59 -0300

    Add AWS IAM policy and role attachment for wallet API in policies.tf

---
+++ terraform/policies.tf
@@ -0,0 +1,33 @@
+resource "aws_iam_policy" "wallet_api_policy" {
+    name         = "Wallet_API_Policy"
+    path         = "/"
+    description  = "AWS IAM Policy for managing aws lambda role"
+    policy = jsonencode(
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Action": [
+                    "logs:CreateLogGroup",
+                    "logs:CreateLogStream",
+                    "logs:PutLogEvents",
+                    ],
+                    "Resource": "arn:aws:logs:*:*:*",
+                    "Effect": "Allow"
+                },
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "lambda:*",
+                    ],
+                    "Resource": "*"
+                },
+            ]
+        }
+    )
+}
+
+resource "aws_iam_role_policy_attachment" "Wallet_API_Attach" {
+ role        = aws_iam_role.wallet_api_role.name
+ policy_arn  = aws_iam_policy.wallet_api_policy.arn
+}
