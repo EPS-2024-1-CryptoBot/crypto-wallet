ad9af4bcfa8a294f0cd933fe5b6c2a2f5a6f715d
Victor Buendia <victorbuendia03@gmail.com>
2024-04-22 15:24:08 -0300

    Update Terraform backend and bucket configurations

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket          = "cryptobotunb-terraform-${var.environment}-state"
+    bucket          = "cryptobotunb-terraform-prod-state"
     key             = "terraform/terraform.tfstate"
     region          = "us-east-1"
-    dynamodb_table  = "cryptobotunb-terraform-${var.environment}-db"
+    dynamodb_table  = "cryptobotunb-terraform-prod-db"
     encrypt         = true
   }
 }
