ca3bef31ad39915b32abeaed7ae710e0a66597ab
Victor Buendia <victorbuendia03@gmail.com>
2024-04-26 20:24:48 -0300

    Add logging configuration to wallet/log.py

---
+++ wallet/log.py
@@ -0,0 +1,14 @@
+import logging
+
+from colors import COLORS
+
+logging.basicConfig(
+    format=f"[{COLORS['CYAN']}%(levelname)s{COLORS['WHITE']}][{COLORS['PURPLE']}%(asctime)s{COLORS['WHITE']}][{COLORS['YELLOW']}%(filename)-15s{COLORS['WHITE']}]{COLORS['DARK_GRAY']}[%(lineno)4d][%(name)10s]{COLORS['WHITE']} - %(message)s",
+    level=logging.INFO,
+    force=True,
+)
+
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    return logger
