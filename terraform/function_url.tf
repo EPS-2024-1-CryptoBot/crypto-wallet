45419c16f699dff311b4fad7f75250f4e0595a64
Victor Buendia <victorbuendia03@gmail.com>
2024-05-08 13:07:48 -0300

    refactor: Add AWS Lambda function for RSA key generation

--- terraform/function_url.tf
+++ terraform/function_url.tf
@@ -2,3 +2,8 @@ resource "aws_lambda_function_url" "wallet_api_url" {
   function_name      = aws_lambda_function.terraform_lambda_func.function_name
   authorization_type = "NONE"
 }
+
+resource "aws_lambda_function_url" "rsa_keygen_api_url" {
+  function_name      = aws_lambda_function.terraform_rsa_keygen.function_name
+  authorization_type = "NONE"
+}
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -9,14 +9,21 @@ resource "aws_lambda_function" "terraform_lambda_func" {
     source_code_hash               = base64sha256(aws_s3_object.file_upload.key)
     timeout                        = 10
 
-    # vpc_config {
-    #     subnet_ids         = [aws_subnet.api_private_subnet.id]
-    #     security_group_ids = [aws_security_group.lambda_internet_access_sec_group.id]
-    # }
-
     environment {
         variables = {
             MONGO_URI = var.MONGO_URI
         }
     }
+}
+
+resource "aws_lambda_function" "terraform_rsa_keygen" {
+    s3_bucket                      = aws_s3_bucket.wallet_api_zip.id
+    s3_key                         = aws_s3_object.file_upload.key
+    function_name                  = "RSA_KeyGen_API"
+    role                           = aws_iam_role.wallet_api_role.arn
+    handler                        = "encryption.handler"
+    runtime                        = "python3.8"
+    depends_on                     = [aws_iam_role_policy_attachment.Wallet_API_Attach]
+    source_code_hash               = base64sha256(aws_s3_object.file_upload.key)
+    timeout                        = 10
 }
